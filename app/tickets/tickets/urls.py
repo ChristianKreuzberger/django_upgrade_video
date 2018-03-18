33f9218ecdee9b21bea443b6dc602ba71e8210a5
ChristianKreuzberger <s4ko88@gmail.com>
2018-03-18 17:07:07 +0100

    added edit and detail view

--- app/tickets/tickets/urls.py
+++ app/tickets/tickets/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 
-from tickets.tickets.views import ListTicketsView, CreateTicketView
+from tickets.tickets.views import ListTicketsView, CreateTicketView, ViewTicketView, EditTicketView
 
 urlpatterns = [
     url(r'^$', ListTicketsView.as_view(), name='list'),
     url(r'^create/$', CreateTicketView.as_view(), name='create'),
+    url(r'^view/(?P<pk>[0-9]+)/$', ViewTicketView.as_view(), name='view'),
+    url(r'^edit/(?P<pk>[0-9]+)/$', EditTicketView.as_view(), name='edit'),
 ]
--- app/tickets/tickets/views.py
+++ app/tickets/tickets/views.py
@@ -1,8 +1,9 @@
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.template import RequestContext
-from django.views.generic import View
+from django.views.generic import View, DetailView
+
 
 from tickets.tickets.forms import CreateTicketForm
 from tickets.tickets.models import Ticket
@@ -19,6 +20,7 @@ class ListTicketsView(View):
         return render(request, self.template_name, context)
 
 
+# toDo: Try out CreateView
 class CreateTicketView(View):
     """
     View for creating a new ticket
@@ -44,6 +46,7 @@ class CreateTicketView(View):
         current_user = request.user
 
         if create_ticket_form.is_valid():
+            # ToDo: Replace with create_ticket_form.save()
             ticket = Ticket.objects.create(
                 title=create_ticket_form.data['title'],
                 description=create_ticket_form.data['description'],
@@ -54,20 +57,45 @@ class CreateTicketView(View):
             # assignee is a M2M field which needs to be filled after we create the ticket
             ticket.assigned_to.add(create_ticket_form.data['assigned_to'])
 
-            # ToDo: redirect to ticket detail view once ready
-            return HttpResponseRedirect(reverse('tickets:list'))
+            return HttpResponseRedirect(reverse('tickets:view', {'pk': ticket.pk}))
 
         return render(request, self.template_name, context)
 
 
-class EditTicketView(View):
-    template_name = ''
+class ViewTicketView(DetailView):
+    template_name = 'tickets/view.html'
 
-    def get(self, request):
+    model = Ticket
+
+
+# toDo: Try out EditView
+class EditTicketView(View):
+    template_name = 'tickets/edit.html'
+
+    def get(self, request, pk=None):
+        ticket = get_object_or_404(Ticket.objects.all(), pk=pk)
+
+        edit_ticket_form = CreateTicketForm(instance=ticket)
+
         context = RequestContext(request, {
+            'ticket': ticket,
+            'edit_ticket_form': edit_ticket_form
         })
 
         return render(request, self.template_name, context)
 
-    def post(self, request, *args, **kwargs):
-        pass
+    def post(self, request, pk, *args, **kwargs):
+        ticket = get_object_or_404(Ticket.objects.all(), pk=pk)
+
+        edit_ticket_form = CreateTicketForm(request.POST or None, instance=ticket)
+
+        context = RequestContext(request, {
+            'ticket': ticket,
+            'edit_ticket_form': edit_ticket_form
+        })
+
+        if edit_ticket_form.is_valid():
+            edit_ticket_form.save()
+            return HttpResponseRedirect(reverse('tickets:view', kwargs={'pk': ticket.pk}))
+
+        return render(request, self.template_name, context)
