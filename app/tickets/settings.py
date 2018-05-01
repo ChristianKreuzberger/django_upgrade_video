f25373e3b6c09fae023b7867378ebc255da970a0
ChristianKreuzberger <s4ko88@gmail.com>
2018-05-01 18:42:01 +0200

    Basic Docker Setup

--- app/tickets/settings.py
+++ app/tickets/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'tickets.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tickets',
+        'USER': 'tickets',
+        'PASSWORD': 'tickets',
+        'HOST': 'postgres',
+        'PORT': '5432',
     }
 }
 
